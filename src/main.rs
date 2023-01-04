086d075d6a306cdb191bba38db54be86bee6786e
Quentin Moss <quentin.moss@protonmail.com>
2023-01-04 10:57:42 -0800

    Create health check and test

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}", &name)
-}
+use zero2hero::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new( || {
-        App::new()
-            // web::get() is a short-cut for Route::new().guard(guard::Get()) a.k.a. the request should be passed to
-            // the handler if and only if its HTTP method is GET.
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    let address = TcpListener::bind("127.0.0.1:8080")?;
+    run(address)?.await
 }
